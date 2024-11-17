76a3218919df14f1133a11bd43e9da5b4853784c
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-11-17 22:20:02 +0700

    [add] env

--- src/app/api/repository/PredictionRepository.ts
+++ src/app/api/repository/PredictionRepository.ts
@@ -1,15 +1,18 @@
 export const predictBellPepperHealthy = async (data: File) => {
-    let formData = new FormData();
-    formData.append('file', data);
-    try {
-        const response = await fetch(`http://127.0.0.1:5000/predict`, {
-            method: 'POST',
-            body: formData,
-        });
-        return await response.json();
-    }catch (error) {
-        console.log(error);
-        alert(error);
-        return null
-    }
+  let formData = new FormData();
+  formData.append("file", data);
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}predict`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    alert(error);
+    return null;
+  }
 };
